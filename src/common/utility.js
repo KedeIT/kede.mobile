7cc00e02c14b40c4aeb48b8680dcca212b58c342
Artech Chu <finalclassic@outlook.com>
2018-09-30 17:29:10 +0800

    add paged function in utility.js

--- src/common/utility.js
+++ src/common/utility.js
@@ -1,3 +1,4 @@
+import {List as ImmutableList} from 'immutable';
 const getWindowScroll = () => {
     let supportPageOffset = window.pageXOffset !== undefined;
     let isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
@@ -25,3 +26,24 @@ export const autoWindowScroll = (path) => {
     if (!scrollInfo) return;
     window.scrollTo(scrollInfo.x, scrollInfo.y);
 }
+
+/*页面级分页数据:
+    dataList:需分页原数据
+    pageSize:页尺寸
+    pageNo:页码
+*/
+export const getPagedData = (dataList, pageSize, pageNo) => {
+
+    if (!dataList || dataList.length === 0 || pageNo < 1 || pageSize <= 0) return null;
+
+    if (ImmutableList.isList(dataList)) {
+        console.log("immutable list");
+        return dataList.skip((pageNo - 1) * pageSize).take(pageSize);
+    }
+    //以下为js和immutable object 通用部分
+    //todo：性能测试，看slice和skip+take哪个性能更高
+    let start = pageSize * (pageNo - 1);
+    let end = pageNo * pageSize;
+    return dataList.slice(start, end);
+}
+
