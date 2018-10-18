4c8d4269e42251ed28521b3f809d20e953e173ae
Artech Chu <finalclassic@outlook.com>
2018-10-18 17:04:13 +0800

    context demo2: transport parent methods to child

--- src/pages/mine/components/ThemeButton.js
+++ src/pages/mine/components/ThemeButton.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 import { ThemeButtonContext } from '../context/ThemeButtonContext';
 class ThemeButton extends Component {
@@ -6,10 +6,13 @@ class ThemeButton extends Component {
         return (
             <ThemeButtonContext.Consumer>
                 {
-                    importedValue => (
-                        <div style={{ color: importedValue }} {...this.props}>当前按钮的颜色  ：
-                                    <span>{importedValue}</span>
-                        </div>
+                    (importedValue) => (
+                        <Fragment>
+                            <div style={{ color: importedValue.currentColor }} {...this.props}>当前按钮的颜色  ：
+                                    <span>{importedValue.currentColor}</span>
+                            </div>
+                            <button onClick={importedValue.toggleColor} >更改颜色,当前为{importedValue.currentColor}</button>
+                        </Fragment>
                     )
                 }
 
--- src/pages/mine/index.js
+++ src/pages/mine/index.js
@@ -2,41 +2,27 @@ import React, { Component } from 'react'
 import List from './components/List';
 
 import { ThemeButtonContext, style } from './context/ThemeButtonContext';
-
 class Mine extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            currentColor: style.colorRed
+            currentColor: style.colorRed,
+            toggleColor:this.toggleColor
         }
-        // this.toggleColor2 = this.toggleColor2.bind(this);
     }
 
     render() {
         return (
             <div style={{ height: "600px", margin: "50% auto" }}>
                 Mine page
-                <ThemeButtonContext.Provider value={this.state.currentColor}>
+                <ThemeButtonContext.Provider value={this.state} >
                     <List></List>
                 </ThemeButtonContext.Provider>
-                <button onClick={ this.toggleColor} my="自定义的">父级Mine更改按钮颜色,当前为{this.state.currentColor}</button>
             </div>);
     }
 
     toggleColor = () => {
-        if (this.state.currentColor == style.colorRed)
-            this.setState({
-                currentColor: style.colorYellow
-            });
-        else
-            this.setState({
-                currentColor: style.colorRed
-            });
-    }
-
-    toggleColor2(a,b,e) {
-
         if (this.state.currentColor == style.colorRed)
             this.setState({
                 currentColor: style.colorYellow
