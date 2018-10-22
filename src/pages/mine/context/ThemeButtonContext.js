3a27ddf14405432a3846cbd7981080db4ebd2b4f
Artech Chu <finalclassic@outlook.com>
2018-10-22 11:31:23 +0800

    还原测试用context文件

--- src/pages/mine/context/ThemeButtonContext.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const style={
-    colorRed:"red",
-    colorYellow:"yellow",
-    colorDefault:"blue"
-}
-
-export const ThemeButtonContext = React.createContext(style.colorDefault);
--- src/pages/mine/index.js
+++ src/pages/mine/index.js
@@ -1,41 +1,20 @@
 import React, { Component } from 'react'
-import List from './components/List';
-import FancyBox from '../../components/fancyBox';
-import { ThemeButtonContext, style } from './context/ThemeButtonContext';
 class Mine extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            currentColor: style.colorRed,
-            toggleColor: this.toggleColor
-        }
+
     }
 
     render() {
-        let themeButton = (
-            <ThemeButtonContext.Provider value={this.state} >
-                <List></List>
-            </ThemeButtonContext.Provider>
-        );
+
         return (
             <div style={{ height: "600px", margin: "50% auto" }}>
                 Mine page
-
-                <FancyBox topContent={themeButton} bottomContent={<span>HAHA BOTTOM CONTENT HERE</span>}></FancyBox>
             </div>);
     }
 
-    toggleColor = () => {
-        if (this.state.currentColor == style.colorRed)
-            this.setState({
-                currentColor: style.colorYellow
-            });
-        else
-            this.setState({
-                currentColor: style.colorRed
-            });
-    }
+
 }
 
 export default Mine;
